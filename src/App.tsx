d9fc05ae523503702d8bd9ab80f0807618c93abd
Halazv2 <halaziani2003@gmail.com>
2023-01-02 11:06:42 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./pages";
+import Navbar from "./layouts/Navbar";
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
